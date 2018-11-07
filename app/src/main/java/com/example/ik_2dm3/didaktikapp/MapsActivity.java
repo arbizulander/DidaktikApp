534d12bde7d227fa94f05a4dcc10ad2772e49923
iñaki <arrigoinaki@gmail.com>
2018-11-07 13:09:23 +0100

    Geolocalizacion terminada

--- app/src/main/java/com/example/ik_2dm3/didaktikapp/MapsActivity.java
+++ app/src/main/java/com/example/ik_2dm3/didaktikapp/MapsActivity.java
@@ -1,42 +1,174 @@
 package com.example.ik_2dm3.didaktikapp;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.OnMapReadyCallback;
+import android.util.Log;
 
 
-public class MapsActivity extends AppCompatActivity {
-    private MapView mapView;
+
+
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+
+
+
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+
+
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
+import java.util.List;
+
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,LocationEngineListener,
+        PermissionsListener {
+    private com.mapbox.mapboxsdk.maps.MapView mapView;
+    private  MapboxMap map;
+    private PermissionsManager permissionsManager;
+    private LocationEngine locationEngine;
+    private LocationLayerPlugin locationLayerPlugin;
+    private Location originLocation;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*Mapbox.getInstance(this, "pk.eyJ1IjoiZGlkYWt0aWthcHAiLCJhIjoiY2pubGdlOXVpMTF3MDN3czVhNTJ4eWV2NCJ9.axDKMhplxotNcdvMT9y6dg");
-        setContentView(R.layout.activity_main);
+        Mapbox.getInstance(this, "pk.eyJ1IjoiZGlkYWt0aWthcHAiLCJhIjoiY2pubGdlOXVpMTF3MDN3czVhNTJ4eWV2NCJ9.axDKMhplxotNcdvMT9y6dg");
+        setContentView(R.layout.activity_maps);
+
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
 
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-                mapboxMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(43.356888, -3.0114555))
-                        .title("Titulo")
-                        .snippet("lalala"));
-
-                mapboxMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(43.456888, -3.0114555))
-                        .title("Titulo2")
-                        .snippet("lololo"));
-
-            }
-        });*/
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+    }
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+
+        map = mapboxMap;
+        enableLocation();
+
+    }
+
+    private void enableLocation(){
+
+        if(PermissionsManager.areLocationPermissionsGranted(this)){
+            initializeLocationEngine();
+            initializeLocationLayer();
+
+
+        }else{
+            permissionsManager = new PermissionsManager((this));
+            permissionsManager.requestLocationPermissions(this);
+        }
+
+    }
+    @SuppressWarnings("MissingPermission")
+    private void initializeLocationEngine(){
+
+        locationEngine = new LocationEngineProvider(this).obtainBestLocationEngineAvailable();
+        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.activate();
+
+        Location lastLocation = locationEngine.getLastLocation();
+        if(lastLocation != null){
+
+            originLocation = lastLocation;
+            setCameraPosition(lastLocation);
+
+
+        }else{
+            locationEngine.addLocationEngineListener(this);
+
+        }
+
+    }
+
+
+    @SuppressWarnings("MissingPermission")
+    private void initializeLocationLayer(){
+
+        locationLayerPlugin = new LocationLayerPlugin(mapView, map, locationEngine);
+        locationLayerPlugin.setLocationLayerEnabled(true);
+        locationLayerPlugin.setCameraMode(com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode.TRACKING_COMPASS);
+        locationLayerPlugin.setRenderMode(RenderMode.COMPASS);
+
+    }
+
+    private void setCameraPosition(Location location){
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),
+                location.getLongitude()), 13.0));
+
+    }
+
+
+    @Override
+    @SuppressWarnings("MissingPermission")
+    public void onConnected() {
+        locationEngine.requestLocationUpdates();
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        if(location != null){
+            originLocation = location;
+            setCameraPosition(location);
+        }
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if(granted){
+            enableLocation();
+        }
+
+    }
+
+
+
+    /**********************************************************/
+
+    @SuppressWarnings("MissingPermission")
     @Override
     public void onStart() {
         super.onStart();
+        if(locationEngine != null){
+            locationEngine.requestLocationUpdates();
+        }
+        if(locationLayerPlugin != null){
+            locationLayerPlugin.onStart();
+        }
         mapView.onStart();
     }
 
@@ -55,6 +187,12 @@ public class MapsActivity extends AppCompatActivity {
     @Override
     public void onStop() {
         super.onStop();
+        if(locationEngine != null){
+            locationEngine.removeLocationUpdates();
+        }
+        if(locationLayerPlugin != null){
+            locationLayerPlugin.onStop();
+        }
         mapView.onStop();
     }
 
@@ -67,13 +205,13 @@ public class MapsActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if(locationEngine != null){
+            locationEngine.deactivate();
+        }
         mapView.onDestroy();
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
+
+
 
 }
