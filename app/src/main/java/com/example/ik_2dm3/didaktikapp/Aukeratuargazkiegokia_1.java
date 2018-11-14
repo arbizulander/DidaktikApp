c7c5a5275944278be99aef1aff78d9ac94249ed6
Lander <arbizulander@gmail.com>
2018-11-14 10:24:08 +0100

    juego 1_1 (en proceso)

--- app/src/main/java/com/example/ik_2dm3/didaktikapp/Aukeratuargazkiegokia_1.java
+++ app/src/main/java/com/example/ik_2dm3/didaktikapp/Aukeratuargazkiegokia_1.java
@@ -4,9 +4,11 @@ import android.os.Handler;
 import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,18 +20,24 @@ public class Aukeratuargazkiegokia_1 extends AppCompatActivity {
 
     //las imágenes
     private int imagenes[];
+    private int imagen_correcta;
     //se guardan duplicadas en un array
-    private ImageButton[] array_botones = new ImageButton[3];
+    private ImageButton array_botones[];
 
     //para barajar
     //el ArrayList que recoge el resultado de barajar
     private ArrayList<Integer> arrayAleatorio;
+    private ArrayList<Integer> arrayImagenes;
+
+    ArrayList<Integer> arrayBarajado;
 
     //durante un segundo se bloquea el juego y no se puede pulsar ningún botón
     boolean bloqueo = false;
 
     //para controlar las pausas del juego
     //final Handler handler = new Handler();
+
+    ImageButton primero;
 
 
 
@@ -38,33 +46,40 @@ public class Aukeratuargazkiegokia_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aukeratu_argazkia_egokia_1);
         cargarImagenes();
-        cargarBotones();
+        //cargarBotones();
         //iniciar();
 
-        img1.setImageResource(R.drawable.juego1_1);
+        /*img1.setImageResource(R.drawable.juego1_1);
         img2.setImageResource(R.drawable.juego1_2);
         img3.setImageResource(R.drawable.juego1_3);
-
-        //iniciar();
+*/
+        iniciar();
     }
 
     public void cargarImagenes(){
-        imagenes = new int[]{
+
+       imagenes = new int[]{
                 R.drawable.juego1_1,
                 R.drawable.juego1_2,
                 R.drawable.juego1_3
         };
+       imagen_correcta = R.drawable.juego1_3;
 
         //fondo = R.drawable.fondo;
     }
 
-    /*public ArrayList<Integer> barajar(int longitud) {
+    public ArrayList<Integer> barajar(int longitud) {
         ArrayList resultadoA = new ArrayList<Integer>();
-        for(int i=0; i<longitud; i++)
-            resultadoA.add(i % longitud/2);
+        for(int i=0; i<longitud; i++) {
+
+            if (!resultadoA.contains(imagenes[i % longitud / 2])) {
+                resultadoA.add(imagenes[i % longitud / 2]);
+                //Log.d("mytag","ya lo tiene");
+            }
+        }
         Collections.shuffle(resultadoA);
         return  resultadoA;
-    }*/
+    }
 
     public void cargarBotones(){
         img1 = (ImageButton) findViewById(R.id.imageButton);
@@ -74,31 +89,47 @@ public class Aukeratuargazkiegokia_1 extends AppCompatActivity {
     }
 
     public void iniciar(){
-       //arrayAleatorio = barajar(imagenes.length*2);
-        //cargarBotones();
+       arrayBarajado = barajar(imagenes.length*2);
+        cargarBotones();
+        Log.d("mytag","VALORES IMAGENES: "+ imagenes[0]+"  "+imagenes[1]+"  "+imagenes[2]);
+        Log.d("mytag", "·VALORES ARRAYBARAJADO: "+ arrayBarajado.get(0).intValue()+"  "+arrayBarajado.get(1).intValue()+"   "+arrayBarajado.get(2).intValue());
 
-        Collections.shuffle(Collections.singletonList(imagenes));
+        imagenes = new int []{arrayBarajado.get(0),arrayBarajado.get(1),arrayBarajado.get(2)};
 
+        //Collections.shuffle(imagenes);
         //MOSTRAMOS LA IMAGEN
-        for(int i=0; i<imagenes.length; i++) {
+        for(int i=0; i<array_botones.length; i++) {
             array_botones[i].setScaleType(ImageView.ScaleType.CENTER_CROP);
             array_botones[i].setImageResource(imagenes[i]);
         }
 
 
         //AÑADIMOS LOS EVENTOS A LOS BOTONES DEL JUEGO
-        /*for(int i=0; i <arrayAleatorio.size(); i++){
-            final int j=i;
-            array_imagenes[i].setEnabled(true);
-            array_imagenes[i].setOnClickListener(new View.OnClickListener() {
+        for(int i=0; i <array_botones.length; i++){
+            final int j = i;
+            array_botones[i].setEnabled(true);
+            array_botones[i].setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(!bloqueo) {
-                        //comprobar(j, botonera[j]);
-                    }
+                    comprobar(imagenes[j]);
                 }
             });
-        }*/
+        }
+    }
+
+    public void comprobar(final int ValorImagen){
+
+        if (ValorImagen == imagen_correcta){
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Has  ganado!!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "ERROR!!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
     }
 
 }
--- app/src/main/java/com/example/ik_2dm3/didaktikapp/details_list.java
+++ app/src/main/java/com/example/ik_2dm3/didaktikapp/details_list.java
@@ -133,7 +133,7 @@ public class details_list extends AppCompatActivity {
         int ID_juego = Listado_juegos.get(0).getId_juego();
         String titulo = Listado_juegos.get(0).getNombre_juego();
 
-        String nombre_completo = titulo+"_"+ID_juego;
+        String nombre_completo = titulo+"_"+ID_juego+".class";
         nombre_completo = nombre_completo.replace(" ","");
         Log.d("mytag", "NOMBRE JUEGO: " +nombre_completo);
         int cont = 0;
@@ -141,7 +141,7 @@ public class details_list extends AppCompatActivity {
             //Log.d("mytag", "NOMBRE JUEGO: " +nombre_completo);
         Intent i = null;
         try {
-            i = new Intent(this, Class.forName(nombre_completo.toString()));
+            i = new Intent(this, Class.forName(nombre_completo));
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
