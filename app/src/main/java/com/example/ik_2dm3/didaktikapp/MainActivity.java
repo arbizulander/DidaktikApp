579a9a8f18b26cb206f48d205394446a4b274826
Lander <arbizulander@gmail.com>
2018-12-04 10:45:49 +0100

    Revert "arreglo en galeria, añadir logo y cambios en diseño"
    This reverts commit 02984850d8a4c73826557efb4d2a0032d9997630.

--- app/src/main/java/com/example/ik_2dm3/didaktikapp/MainActivity.java
+++ app/src/main/java/com/example/ik_2dm3/didaktikapp/MainActivity.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnjuego;
 
     //REQ
     private static final int REQ_BTN = 0;
 
+    //Controlador de bases de datos
+    //private MyOpenHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +24,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnStart = findViewById(R.id.btnHasi);
+        //Iniciamos el controlador de la base de datos
+        //db=new MyOpenHelper(this);
 
+        //cogiendo id del boton
+        //botones
+        Button btnStart = findViewById(R.id.btnHasi);
+        btnjuego = findViewById(R.id.button2);
+        //accion al pulsar el boton
         btnStart.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this,menu_main.class);
             AbrirLayout thread = new AbrirLayout(intent);
             thread.start();
             //startActivityForResult(intent, REQ_BTN);
+        });
+
+        btnjuego.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Esaldizuzenaaukeratu_6.class);
+                //Intent intent = new Intent(MainActivity.this,Aukeratuargazkiegokia_1.class);
+                AbrirLayout thread = new AbrirLayout(intent);
+                thread.start();
+                //startActivity(intent);
+            }
         });
     }
 
@@ -39,9 +60,11 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
-            runOnUiThread(() -> {
-                Log.d("mytag", "... ABRIENDO INTENT...");
-               startActivityForResult(i,REQ_BTN);
+            runOnUiThread(new Runnable() {
+                @Override public void run() {
+                    Log.d("mytag", "... ABRIENDO INTENT...");
+                   startActivityForResult(i,REQ_BTN);
+                }
             });
         }
     }
