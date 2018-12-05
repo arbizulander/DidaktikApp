0040c6e0bc0a19a9d097a51a95248787bab4b76c
iñaki <arrigoinaki@gmail.com>
2018-12-05 10:28:31 +0100

    gune 3 jarduera 1 (terminado)

--- app/src/main/java/com/example/ik_2dm3/didaktikapp/Argazkiaaztertu_7.java
+++ app/src/main/java/com/example/ik_2dm3/didaktikapp/Argazkiaaztertu_7.java
@@ -1,13 +1,35 @@
 package com.example.ik_2dm3.didaktikapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 public class Argazkiaaztertu_7 extends AppCompatActivity {
+
+    MediaPlayer mp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_argazkiaaztertu_7);
+
+
+        mp = MediaPlayer.create(this, R.raw.a3_1);
+        mp.start();
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+            // Esto es lo que hace mi botón al pulsar ir a atrás
+            Toast.makeText(getApplicationContext(), "Voy hacia atrás!!",
+                    Toast.LENGTH_SHORT).show();
+            mp.stop();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
