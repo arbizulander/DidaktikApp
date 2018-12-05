7a799d7f83e8be182c84b2f2b54dcdc998c42f71
iñaki <arrigoinaki@gmail.com>
2018-12-05 10:15:20 +0100

    jarduera 3 gune 2 (proceso)

---
+++ app/src/main/java/com/example/ik_2dm3/didaktikapp/komikia_8.java
@@ -0,0 +1,13 @@
+package com.example.ik_2dm3.didaktikapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class komikia_8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_komikia_8);
+    }
+}
