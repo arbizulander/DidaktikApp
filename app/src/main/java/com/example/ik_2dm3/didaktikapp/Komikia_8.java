8494b84e8638e4688e2034877c0e0af22c65ba5a
Lander <arbizulander@gmail.com>
2019-01-11 14:21:50 +0100

    Update Komikia_8.java

--- app/src/main/java/com/example/ik_2dm3/didaktikapp/Komikia_8.java
+++ app/src/main/java/com/example/ik_2dm3/didaktikapp/Komikia_8.java
@@ -5,6 +5,8 @@ import android.graphics.Color;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 
@@ -12,12 +14,25 @@ import android.widget.LinearLayout;
 public class Komikia_8 extends AppCompatActivity {
 
 
-    ViewPager viewPager;
+    private ViewPager viewPager;
+    private ImageButton btnNextGame, btnPreviousGame;
+    private int pag_anterior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_komikia_8);
+
+        btnNextGame = findViewById(R.id.btnNextGame);
+        btnPreviousGame = findViewById(R.id.btnPreviousGame);
+
+        /*btnPreviousGame.setEnabled(false);
+        btnPreviousGame.setVisibility(View.INVISIBLE);
+
+        btnNextGame.setEnabled(false);
+        btnNextGame.setVisibility(View.INVISIBLE);*/
+
+        pag_anterior = getIntent().getIntExtra("pag_anterior", 0);
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
 
