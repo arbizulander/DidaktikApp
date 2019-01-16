f1fe05205b0189561d571f9e593854ddce7eae19
iñaki <arrigoinaki@gmail.com>
2019-01-16 09:28:28 +0100

    manifest juego 14

---
+++ app/src/main/java/com/example/ik_2dm3/didaktikapp/Argazkiakordenatu_14.java
@@ -0,0 +1,13 @@
+package com.example.ik_2dm3.didaktikapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Argazkiakordenatu_14 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_argazkiakordenatu_14);
+    }
+}
