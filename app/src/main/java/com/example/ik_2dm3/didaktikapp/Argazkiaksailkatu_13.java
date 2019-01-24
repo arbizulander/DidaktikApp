9b25d830edcb046c4afb5352a99b3b38e0676811
iñaki <arrigoinaki@gmail.com>
2019-01-24 13:27:32 +0100

    Bloqueo pantalla control juego13

--- app/src/main/java/com/example/ik_2dm3/didaktikapp/Argazkiaksailkatu_13.java
+++ app/src/main/java/com/example/ik_2dm3/didaktikapp/Argazkiaksailkatu_13.java
@@ -1,8 +1,10 @@
 package com.example.ik_2dm3.didaktikapp;
 
+import android.content.BroadcastReceiver;
 import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,10 +29,33 @@ public class Argazkiaksailkatu_13 extends AppCompatActivity {
     private int pag_anterior;
     static final int REQ_BTN = 0;
 
+    BroadcastReceiver miBroadcast = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                Log.i("TAG", "Screen ON");
+            }
+            else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                Log.i("TAG", "Screen OFF");
+                mp.stop();
+                mp2.stop();
+                mp1.stop();
+            }
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_argazkiaksailkatu_13);
+
+        //Aquí registras los eventos que quieres escuchar
+        registerReceiver(miBroadcast, new IntentFilter(Intent.ACTION_SCREEN_ON));
+        registerReceiver(miBroadcast, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+
+
         setTitle("Argazkiak sailkatu");
         btnPreviousGame = findViewById(R.id.btnPreviousGame);
         btnPreviousGame.setEnabled(false);
